5c8c6d65a9042d5c547f0c730efb02583970c441
yakov302 <yakov.hananel@gmail.com>
2022-10-11 19:00:48 +0300

    printing

--- client/src/action_in.c
+++ client/src/action_in.c
@@ -56,26 +56,26 @@ static void give_2_strings(char* buffer, char* first_string, char* second_string
 
 static void log_in_success(User* user, char* buffer)
 {
-    print_message(LOG_IN_SUCCESS);
     char name[STRING_SIZE];
     give_1_strings(buffer, name);
     set_name(user, name);
     set_log_in_status(user, TRUE);
+    print_message(LOG_IN_SUCCESS, name);
 }
 
 static void log_out_success(User* user, char* buffer)
 {
-    print_message(EXIT_CHAT_SUCCESS);
     remove_all_groups_from_user(user);
     set_log_in_status(user, FALSE);
+    print_message(EXIT_CHAT_SUCCESS, user->m_name);
 }
 
 static void new_group(User* user, char* buffer, Message_type message_type)
 {
-    print_message(message_type);
     char group_name[STRING_SIZE];
     char group_ip[STRING_SIZE];
     give_2_strings(buffer, group_name, group_ip);
+    print_message(message_type, group_name);
 
     Group* group = group_create(group_ip, GROUPS_PORT, group_name, name(user));
     add_group_for_user(user, group);
@@ -90,32 +90,37 @@ static void print_existing_groups_name(char* buffer)
 
 static void leave_group(User* user, char* buffer, Message_type message_type)
 {
-    print_message(message_type);
     char group_name[STRING_SIZE];
     give_1_strings(buffer, group_name);
 
     Group* group_ptr = group(user, group_name);
     remove_group_from_user(user, group_ptr);
     group_destroy(group_ptr);
+    
+    print_message(message_type, group_name);
 }
 
 void get_buffer(ActionIn* action_in, char* buffer)
 {
     Message_type message = message_type(buffer);
     action_in->m_lest_message = message;
+    char user_name[STRING_SIZE];
+    char group_name[STRING_SIZE];
 
     switch (message)
     {
         case REGISTRATION_SUCCESS:
-            print_message(REGISTRATION_SUCCESS);
+            give_1_strings(buffer, user_name);
+            print_message(REGISTRATION_SUCCESS, user_name);
             break;
 
         case REGISTRATION_USER_NAME_ALREADY_EXISTS:
-            print_message(REGISTRATION_USER_NAME_ALREADY_EXISTS);
+            give_1_strings(buffer, user_name);
+            print_message(REGISTRATION_USER_NAME_ALREADY_EXISTS, user_name);
             break;
 
         case REGISTRATION_FAIL:
-            print_message(REGISTRATION_FAIL);
+            print_message(REGISTRATION_FAIL, "");
             break; 
 
         case LOG_IN_SUCCESS:
@@ -123,19 +128,20 @@ void get_buffer(ActionIn* action_in, char* buffer)
             break;
 
         case LOG_IN_FAIL:
-            print_message(LOG_IN_FAIL);
+            print_message(LOG_IN_FAIL, "");
             break;
 
         case LOG_IN_WRONG_USER_NAME:
-            print_message(LOG_IN_WRONG_USER_NAME);
+            print_message(LOG_IN_WRONG_USER_NAME, "");
             break;
 
         case LOG_IN_WRONG_PASSWORD:
-            print_message(LOG_IN_WRONG_PASSWORD);
+            print_message(LOG_IN_WRONG_PASSWORD, "");
             break;
 
         case LOG_IN_USER_ALREADY_LOGGED_IN:
-            print_message(LOG_IN_USER_ALREADY_LOGGED_IN);
+            give_1_strings(buffer, user_name);
+            print_message(LOG_IN_USER_ALREADY_LOGGED_IN, user_name);
             break;
 
         case EXIT_CHAT_SUCCESS:
@@ -143,11 +149,11 @@ void get_buffer(ActionIn* action_in, char* buffer)
             break;
 
         case EXIT_CHAT_USER_NOT_EXISTS:
-            print_message(EXIT_CHAT_USER_NOT_EXISTS);
+            print_message(EXIT_CHAT_USER_NOT_EXISTS, "");
             break;
 
         case EXIT_CHAT_FAIL:
-            print_message(EXIT_CHAT_FAIL);
+            print_message(EXIT_CHAT_FAIL, "");
             break;
 
         case OPEN_NEW_GROUP_SUCCESS:
@@ -155,15 +161,16 @@ void get_buffer(ActionIn* action_in, char* buffer)
             break;
 
         case OPEN_NEW_GROUP_FAIL:
-            print_message(OPEN_NEW_GROUP_FAIL);
+            print_message(OPEN_NEW_GROUP_FAIL, "");
             break;
 
         case OPEN_NEW_GROUP_GROUP_NAME_ALREADY_EXISTS:
-            print_message(OPEN_NEW_GROUP_GROUP_NAME_ALREADY_EXISTS);
+            give_1_strings(buffer, group_name);
+            print_message(OPEN_NEW_GROUP_GROUP_NAME_ALREADY_EXISTS, group_name);
             break;
 
         case OPEN_NEW_GROUP_USER_NAME_NOT_EXISTS:
-            print_message(OPEN_NEW_GROUP_USER_NAME_NOT_EXISTS);
+            print_message(OPEN_NEW_GROUP_USER_NAME_NOT_EXISTS, "");
             break;
 
         case PRINT_EXISTING_GROUPS_SUCCESS:
@@ -171,7 +178,7 @@ void get_buffer(ActionIn* action_in, char* buffer)
             break;
 
         case PRINT_EXISTING_GROUPS_NO_GROUPS:
-            print_message(PRINT_EXISTING_GROUPS_NO_GROUPS);
+            print_message(PRINT_EXISTING_GROUPS_NO_GROUPS, "");
             break;
         
         case JOIN_EXISTING_GROUP_SUCCESS:
@@ -179,19 +186,20 @@ void get_buffer(ActionIn* action_in, char* buffer)
             break;
 
         case JOIN_EXISTING_GROUP_FAIL:
-            print_message(JOIN_EXISTING_GROUP_FAIL);
+            print_message(JOIN_EXISTING_GROUP_FAIL, "");
             break;
 
         case JOIN_EXISTING_GROUP_USER_ALREADY_CONNECT:
-            print_message(JOIN_EXISTING_GROUP_USER_ALREADY_CONNECT);
+            give_1_strings(buffer, group_name);
+            print_message(JOIN_EXISTING_GROUP_USER_ALREADY_CONNECT, group_name);
             break;
 
         case JOIN_EXISTING_GROUP_GROUP_NOT_EXISTS:
-            print_message(JOIN_EXISTING_GROUP_GROUP_NOT_EXISTS);
+            print_message(JOIN_EXISTING_GROUP_GROUP_NOT_EXISTS, "");
             break;  
 
         case JOIN_EXISTING_GROUP_USER_NOT_EXISTS:
-            print_message(JOIN_EXISTING_GROUP_USER_NOT_EXISTS);
+            print_message(JOIN_EXISTING_GROUP_USER_NOT_EXISTS, "");
             break;
 
         case LEAVE_GROUP_SUCCESS:
@@ -203,15 +211,15 @@ void get_buffer(ActionIn* action_in, char* buffer)
             break;
 
         case LEAVE_GROUP_FAIL:
-            print_message(LEAVE_GROUP_FAIL);
+            print_message(LEAVE_GROUP_FAIL, "");
             break;
 
         case LEAVE_GROUP_GROUP_NOT_EXISTS:
-            print_message(LEAVE_GROUP_GROUP_NOT_EXISTS);
+            print_message(LEAVE_GROUP_GROUP_NOT_EXISTS, "");
             break;
 
         case LEAVE_GROUP_USER_NOT_EXISTS:
-            print_message(LEAVE_GROUP_USER_NOT_EXISTS);
+            print_message(LEAVE_GROUP_USER_NOT_EXISTS, "");
             break;
         
         case PING_SERVER_TO_CLIENT:
